c995cbbbace3a401e59c93f3ccbad0751a2fec9c
Gabriel Andrade <gaabscps@gmail.com>
2026-07-12 05:30:12 -0300

    feat: mount LivingBackground behind the home

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
+import { LivingBackground } from "./LivingBackground";
 import { Nav } from "./Nav";
 import { Hero } from "./Hero";
 import { Featured } from "./Featured";
@@ -19,6 +20,7 @@ export default function HomeContent() {
 
   return (
     <Box className="bg-textured" minH="100vh" color="brand.text" display="flex" flexDirection="column">
+      <LivingBackground />
       <Nav active="work" />
       <Box maxW="1200px" w="100%" mx="auto" flex="1">
         <Hero />
